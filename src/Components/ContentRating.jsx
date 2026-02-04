98de0cbfb860b27ea9cf99582a5fbf7fa5da3ed1
wogutierrez@gmail.com <wogutierrez@gmail.com>
2026-02-04 10:57:30 -0500

    Step 3

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,32 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+        likes: 0,
+        dislikes: 0
+    };
+
+
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     
+     <div className='content-rating'></div>
+        <p>
+            //Add text here
+        </p>
+         <div className='rating-buttons'>
+            <button className="like-buttons">
+                Like ({this.state.likes})
+            </button>
+
+            <button className="dislike-button">
+                Dislike ({this.state.dislikes})
+            </button>
+
+         </div>
      </>
     );
   }
